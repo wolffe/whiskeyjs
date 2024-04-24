f1f07154ecb4375bbd8cfdbd10be0175868f4026
Ciprian Popescu <wolffe@users.noreply.github.com>
2024-04-24 18:05:04 +0100

    Update curtain.js

--- js-art/curtain.js
+++ js-art/curtain.js
@@ -1,9 +1,9 @@
 c.style.background = '#0f7';
 c.width = 900;
 
-x.beginPath(); // Begin a new path
+x.beginPath();
 for (n = 4000; n--;) {
     x.lineTo(450 + func_S(n * 94.9 + t / 6) * 450, 250 + func_C(n) * 79 * (4 + func_S(t * 8 + n) / 2));
 }
-x.fillStyle = 'rgba(255, 0, 0, 0.5)'; // Set fill color and opacity
-x.fill(); // Fill the current path
+x.fillStyle = 'rgba(255, 0, 0, 0.5)';
+x.fill();
