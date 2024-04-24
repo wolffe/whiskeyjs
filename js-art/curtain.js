e4e37f2efb57f6adcb08b6a30b2177e3f18bf1e0
Ciprian Popescu <wolffe@users.noreply.github.com>
2024-04-24 18:03:58 +0100

    Update curtain.js

--- js-art/curtain.js
+++ js-art/curtain.js
@@ -1,8 +1,5 @@
 c.style.background = '#0f7';
 c.width = 900;
-for (n = 4000; n--;) {
-  x.lineTo(450 + func_S(n * 94.9 + t / 6) * 450, 250 + func_C(n) * 79 * (4 + func_S(t * 8 + n) / 2));
-}
 
 x.beginPath(); // Begin a new path
 for (n = 4000; n--;) {
