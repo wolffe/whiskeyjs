590197213c8dff9c23155a346280cc6a92245c07
Ciprian Popescu <wolffe@users.noreply.github.com>
2024-04-24 17:59:58 +0100

    Update curtain.js

--- js-art/curtain.js
+++ js-art/curtain.js
@@ -1,5 +1,6 @@
 c.style.background = '#0f7';
 c.width = 900;
-x = c.getContext("2d");
-for (n = 4e3; n--;) x.lineTo(450 + func_S(n * 94.9 + t / 6) * 450, 250 + func_C(n) * 79 * (4 + func_S(t * 8 + n) / 2));
-c.getContext('2d').fill().toString();
+for (n = 4000; n--;) {
+  x.lineTo(450 + func_S(n * 94.9 + t / 6) * 450, 250 + func_C(n) * 79 * (4 + func_S(t * 8 + n) / 2));
+}
+x.fill().toString();
